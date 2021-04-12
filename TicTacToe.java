03906b61e39912ad28aa83fc13243362b762f71d
Tal Shvartzberg <82408347+talshva@users.noreply.github.com>
2021-04-13 00:42:58 +0300

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class TicTacToe {
+class TicTacToe {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String grid [] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
